935650ccbcb3633173a6776f8148e9bc50f31252
taserbeat <taserbeat@gmail.com>
2020-11-03 22:42:30 +0900

    Loginコンポーネントのthemeを設定

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -11,7 +11,42 @@ import Container from '@material-ui/core/Container';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { START_FETCH, FETCH_SUCCESS, ERROR_CATCHED, INPUT_EDIT, TOGGLE_MODE } from './actionTypes'
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3),
+    },
+    span: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: 'teal',
+    },
+    spanError: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: 'fuchsia',
+        marginTop: 10,
+    },
+}));
+
 const Login = () => {
+    const classes = useStyles();
+
     return (
         <div>
 
