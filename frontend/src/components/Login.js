b12fecc0ab5475ead50de8bdbb3de7742fd9c009
taserbeat <taserbeat@gmail.com>
2020-11-03 22:57:42 +0900

    ログイン用のReducerを追加

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -44,8 +44,58 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const initialState = {
+    isLoading: false,
+    isLoginView: true,
+    error: '',
+    credentialsLog: {
+        email: '',
+        password: ''
+    },
+};
+
+const loginReducer = (state, action) => {
+    switch (action.type) {
+        case START_FETCH: {
+            return {
+                ...state,
+                isLoading: true,
+            };
+        }
+        case FETCH_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+            };
+        }
+        case ERROR_CATCHED: {
+            return {
+                ...state,
+                error: 'Email or password is not correct !',
+                isLoading: false,
+            };
+        }
+        case INPUT_EDIT: {
+            return {
+                ...state,
+                [action.inputName]: action.payload,
+                error: '',
+            };
+        }
+        case TOGGLE_MODE: {
+            return {
+                ...state,
+                isLoginView: !state.isLoginView,
+            };
+        }
+        default:
+            return state;
+    }
+}
+
 const Login = () => {
     const classes = useStyles();
+    const [state, dispatch] = useReducer(loginReducer, initialState);
 
     return (
         <div>
