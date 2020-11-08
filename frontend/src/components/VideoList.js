ec6d9fa322fcb9383b66b7ded57a71431c79bdbc
taserbeat <taserbeat@gmail.com>
2020-11-08 20:50:29 +0900

    VideoList.jsを修正

--- frontend/src/components/VideoList.js
+++ frontend/src/components/VideoList.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ApiContext } from '../contexts/ApiContext';
+import Grid from '@material-ui/core/Grid';
+import VideoItem from './VideoItem';
 
 const VideoList = () => {
-    return (
-        <div>
+    const { videos } = useContext(ApiContext);
+    const listOfVideos = videos.map((video) => (
+        <VideoItem key={video.id} video={video} />
+    ));
 
-        </div>
+    return (
+        <Grid container spacing={5}>
+            <div className="video-list">{listOfVideos}</div>
+        </Grid>
     )
 }
 
